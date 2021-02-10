a3c48c94245013078935d950139e790af771a684
NiteshTyagi <nitesh.tyagi.cs.2015@miet.ac.in>
2021-02-11 02:55:48 +0530

    [ADD] 101

---
+++ linkedlist/problem_101.py
@@ -0,0 +1,48 @@
+from problem_1 import Linklist,Node
+
+class Linklist(Linklist):
+
+    def getMiddle(self):
+        ptr = self.head
+        count=0
+        while ptr:
+            count+=1
+            ptr = ptr.next
+        
+        return (count//2)
+    
+    def MiddleToHead(self):
+        if not self.head:
+            return None
+        middle = self.getMiddle()
+        
+        ptr = self.head
+        count = 0
+        while ptr:
+            if count==middle:
+                break
+            prev = ptr
+            ptr = ptr.next
+            count+=1
+        
+        prev.next = ptr.next
+        ptr.next = self.head
+        self.head = ptr
+        return self.head
+        
+
+if __name__ == "__main__":
+
+    ll1 = Linklist()
+    ll1.pushEnd(1)
+    ll1.pushEnd(2)
+    ll1.pushEnd(3)
+    ll1.pushEnd(4)
+    ll1.pushEnd(5)
+    ll1.pushEnd(6)
+    
+    ll1.printList()
+    
+    ll1.MiddleToHead()
+    
+    ll1.printList()
