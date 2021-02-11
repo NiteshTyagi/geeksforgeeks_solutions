e2efb60a425a0a23bc8b7ac8357a20bcc88f2631
NiteshTyagi <nitesh.tyagi.cs.2015@miet.ac.in>
2021-02-12 04:15:13 +0530

    [ADD] josephus problem

---
+++ linkedlist/josephus_problem.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, val=None:int):
+        self.val = val
+        self.next = None
+
+def getJosephusPosition(m, n):
+    
+    head = Node(1)
+    prev = head
+    for i in range(2, n + 1):
+        prev.next = Node(i)
+        prev = prev.next
+    prev.next = head  # Connect last
+    
+
+    
+    ptr1 = head
+    ptr2 = head
+    # print(ptr1.data,ptr2.data,sep='<---111---->')
+    while (ptr1.next != ptr1):
+        count = 1
+        while (count != m):
+            ptr2 = ptr1
+            ptr1 = ptr1.next
+            count += 1
+
+        ptr2.next = ptr1.next
+        
+        ptr1 = ptr2.next
+
+    print("Last person left standing (Josephus Position) is ", ptr1.data)
+
+
+if __name__ == '__main__':
+    n = 14
+    m = 2
+    getJosephusPosition(m, n)
+
