1f03cfe3749a454a8e34a385bfe52c69c7d1c4a0
NiteshTyagi <nitesh.tyagi.cs.2015@miet.ac.in>
2021-02-13 12:03:15 +0530

    [ADD] Deletion in Doubly link list

--- linkedlist/doubly_linkedlist.py
+++ linkedlist/doubly_linkedlist.py
@@ -9,7 +9,6 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-
     def pushFront(self,val:int):
         node = Node(val)
 
@@ -20,8 +19,6 @@ class DoublyLinkedList:
         
         self.head = node
         return node
-
-
 
     def pushEnd(self,val:int):
         node = Node(val)
@@ -37,7 +34,6 @@ class DoublyLinkedList:
             temp.next = node
         return node
     
-
     def pushBeforenode(self,val,next_node):
         node = Node(val)
         if next_node is None:
@@ -57,7 +53,6 @@ class DoublyLinkedList:
             self.head = node
             return node
         
-
     def pushAfternode(self,val,prev_node):
         node = Node(val)
         if prev_node is None:
@@ -80,7 +75,61 @@ class DoublyLinkedList:
         else:
             print('---- List is empty-----')
 
+    def deletionFront(self):
+        if not self.head:
+            print('<----- DeletionFront Linked List is empty ----->')
+        else:
+            temp = self.head
+            if temp:
+                if temp.next:
+                    temp.next.prev = None
+                    self.head = temp.next
+                else:
+                    self.head = None
+        return self.head
 
+    def deletionEnd(self):
+        if not self.head:
+            print('<----- DeletionEnd Linked List is empty ----->')
+        else:
+            temp = self.head
+            while temp and temp.next:
+                temp = temp.next
+            if temp.prev:
+                temp.prev.next = None
+            else:
+                self.head = None
+        return self.head
+
+    def deletionBeforeNode(self,after_node):
+        if after_node is None:
+            print('<----- After Node does not exist ---->')
+        else:
+            if after_node.prev:
+                if after_node.prev.prev:
+                    after_node.prev.prev.next = after_node
+                else:
+                    after_node.prev = None
+                    self.head = after_node
+            else:
+                self.head = None
+        return self.head
+
+    def deletionAfterNode(self,before_node):
+        if before_node is None:
+            print('<----- Before Node does not exist ---->')
+        else:
+            if before_node.next:
+                if before_node.next.next:
+                    before_node.next.next.prev = before_node
+                    before_node.next = before_node.next.next
+                else:
+                    before_node.next = None
+            else:
+                print('-----No node exist after this node-----')
+        return self.head
+                
+                
 if __name__ == '__main__':
 
     dl = DoublyLinkedList()
@@ -88,19 +137,22 @@ if __name__ == '__main__':
     dl.pushFront(4)
     dl.pushFront(2)
     dl.pushFront(5)
-
     dl.printList()
 
     dl.pushEnd(10)
     afternode = dl.pushEnd(7)
     dl.pushEnd(9)
-
     dl.printList()
 
     beforenode = dl.pushAfternode(19,afternode)
-
     dl.printList()
 
     dl.pushBeforenode(21,beforenode)
+    dl.printList()
 
+    dl.deletionFront()
+    dl.deletionEnd()
+
+    dl.deletionBeforeNode(beforenode)
+    dl.deletionAfterNode(afternode)
     dl.printList()
