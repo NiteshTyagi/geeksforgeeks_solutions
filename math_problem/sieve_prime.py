5be6158f71713fc40396c18353fc0fe48938126a
NiteshTyagi <nitesh.tyagi.cs.2015@miet.ac.in>
2021-03-20 15:33:16 +0530

    [ADD] prime number

---
+++ math_problem/sieve_prime.py
@@ -0,0 +1,18 @@
+def sieve_eratosthenes(start,end):
+    result = [[i,True] for i in range(end+1)]
+    p=2
+    while p*p<=end:
+        if result[p][1]:
+            for j in range(p*p,end+1,p):
+                result[j][1]=False
+        p+=1
+    for i in range(start-1,end+1):
+        if result[i][1]:
+            print(result[i][0])
+
+if __name__ == '__main__':
+    # https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+    start = int(input('Enter the starting range of the number:'))
+    end = int(input('Enter the Ending Number:'))
+
+    sieve_eratosthenes(start,end)
